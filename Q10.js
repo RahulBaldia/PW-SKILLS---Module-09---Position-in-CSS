92770c7f16256a05744ca9d28aeceabe50730292
RahulBaldia <151762100+RahulBaldia@users.noreply.github.com>
2024-05-30 17:18:11 +0530

    Assignment  - Modul 09 Position in CSS

---
+++ Q10.js
@@ -0,0 +1,34 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the checkbox and submit button
+var checkbox = document.getElementById("agree");
+var submitBtn = document.getElementById("submitBtn");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Enable/disable the submit button based on the checkbox
+checkbox.onchange = function() {
+    submitBtn.disabled = !checkbox.checked;
+}
